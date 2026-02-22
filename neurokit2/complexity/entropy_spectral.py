a2824a86c884c0904541e996699efd5328467abf
DominiqueMakowski <dom.mak19@gmail.com>
2026-02-22 09:55:36 +0000

    Update entropy_spectral.py

--- neurokit2/complexity/entropy_spectral.py
+++ neurokit2/complexity/entropy_spectral.py
@@ -92,7 +92,7 @@ def entropy_spectral(signal, bins=None, show=False, **kwargs):
         psd = psd.groupby(pd.cut(psd["Frequency"], bins=bins), observed=False).agg(
             "sum"
         )
-        idx = psd.index.values.astype(str)
+        idx = np.array(psd.index, dtype=str)
     else:
         idx = psd["Frequency"].values
 
@@ -102,7 +102,7 @@ def entropy_spectral(signal, bins=None, show=False, **kwargs):
 
     if show is True:
         plt.bar(idx, psd["Power"])
-        if not np.issubdtype(idx.dtype, np.floating):
+        if not pd.api.types.is_float_dtype(idx):
             plt.xticks(rotation=90)
         plt.title("Normalized Power Spectrum")
         plt.xlabel("Frequency (Hz)")
